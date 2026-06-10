4700980202b71d7198a1079b527021997b66e3f9
Mike Vanbuskirk <mike@mikevanbuskirk.io>
2026-06-10 16:36:30 -0400

    init.el
    - nix + lsp
    - correct roam reference
    - rss reader

--- emacs/.config/doom/init.el
+++ emacs/.config/doom/init.el
@@ -152,12 +152,13 @@
        ;;lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
-       ;;nix               ; I hereby declare "nix geht mehr!"
+       (nix
+        +lsp)               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org
         +dragndrop
         +download
-        +roam2)               ; organize your plain life in plain text
+        +roam)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
@@ -192,7 +193,7 @@
        ;;emms
        ;;everywhere        ; *leave* Emacs!? You must be joking
        ;;irc               ; how neckbeards socialize
-       ;;(rss +org)        ; emacs as an RSS reader
+       (rss)        ; emacs as an RSS reader
        ;;twitter           ; twitter client https://twitter.com/vnought
 
        :config
