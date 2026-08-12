7474519a71c611edfe211d0d79d54273da3dbd4a
Mike Vanbuskirk <mike@mikevanbuskirk.io>
2026-08-12 17:25:14 -0400

    remove treemacs

--- emacs/.config/doom/config.el
+++ emacs/.config/doom/config.el
@@ -76,7 +76,7 @@
 
 (setq doom-theme 'catppuccin)
 (setq catppuccin-flavor 'mocha)
-(setq doom-themes-treemacs-theme "doom-colors")
+;;(setq doom-themes-treemacs-theme "doom-colors")
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
